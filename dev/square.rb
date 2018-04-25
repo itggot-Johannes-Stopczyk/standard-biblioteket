c843b194d93971c7a93c54d7c6fc0f38bf944381
itggot-Johannes-Stopczyk <johannes.stopczyk@itggot.se>
2018-04-25 14:18:36 +0200

    Added file, code and documentation

---
+++ dev/square.rb
@@ -0,0 +1,13 @@
+# Public: Squares the numer
+#
+# num - The integer number to square.
+#
+# Examples
+#
+#   square(3)
+#   # => 9
+#
+# Returns the added integer.
+def square(num)
+    return num ** 2
+end
