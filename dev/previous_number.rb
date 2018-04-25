997fc0622e1bbf7b056d70bf0542602051378b44
itggot-Johannes-Stopczyk <johannes.stopczyk@itggot.se>
2018-04-25 14:14:22 +0200

    Added code and documentation

---
+++ dev/previous_number.rb
@@ -0,0 +1,13 @@
+# Public: Subtracts 1 from any numer
+#
+# num - The integer number to subtract 1 from.
+#
+# Examples
+#
+#   previous_number(4)
+#   # => 3
+#
+# Returns the added integer.
+def previous_number(num)
+    return num + - 1
+end
