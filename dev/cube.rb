0cb8bde8472a162c5ac644b8919d8a01402e1f2e
itggot-Johannes-Stopczyk <johannes.stopczyk@itggot.se>
2018-04-25 14:21:25 +0200

    Added file, code and documentation

---
+++ dev/cube.rb
@@ -0,0 +1,13 @@
+# Public: Outputs the cube of the integer number
+#
+# num - The integer number to cube.
+#
+# Examples
+#
+#   cube(3)
+#   # => 27
+#
+# Returns the added integer.
+def cube(num)
+    return num ** 3
+end
