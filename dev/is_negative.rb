a046c47bc9e723b7c5fb79c7be8592179bde3600
itggot-Johannes-Stopczyk <johannes.stopczyk@itggot.se>
2018-04-25 14:27:21 +0200

    Added file, code and documentation

---
+++ dev/is_negative.rb
@@ -0,0 +1,14 @@
+# Public: Checks if the number is negative.
+#
+# num - The integer number to check if negative.
+#
+# Examples
+#
+#   square(-3)
+#   # => true
+#
+# Returns if the value is negative(true) or not(false).
+def is_negative(num)
+    return true if num < 0
+    return false
+end
