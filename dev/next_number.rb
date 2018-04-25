28f255dbc59f825bbe0f8a4c6ab3c55f0b63a52c
itggot-Johannes-Stopczyk <johannes.stopczyk@itggot.se>
2018-04-25 14:10:20 +0200

    Added code and documentation

---
+++ dev/next_number.rb
@@ -0,0 +1,13 @@
+# Public: Adds 1 to any numer
+#
+# num - The integer number to add 1 to.
+#
+# Examples
+#
+#   next_number(4)
+#   # => 5
+#
+# Returns the added integer.
+def next_number(num)
+    return num + 1
+end
