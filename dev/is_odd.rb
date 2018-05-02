b29361da99040bd46b282bdfa9dbe3b1ee50e710
itggot-Johannes-Stopczyk <johannes.stopczyk@itggot.se>
2018-05-02 13:15:23 +0200

    Made file, added code and documentation

---
+++ dev/is_odd.rb
@@ -0,0 +1,14 @@
+# Public: Checks if number is odd
+#
+# num - The integer number to check if odd.
+#
+# Examples
+#
+#   square(3)
+#   # => true
+#
+# Returns if the value is odd or not.
+def is_odd(num)
+    return true if num % 2 == 1
+    return false
+end
