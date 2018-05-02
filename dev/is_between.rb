d656c39a793ff31ca52a1f94cda7e3c134b664e6
itggot-Johannes-Stopczyk <johannes.stopczyk@itggot.se>
2018-05-02 13:48:17 +0200

    Corrected documentation

---
+++ dev/is_between.rb
@@ -0,0 +1,21 @@
+# Public: Checks if a number is between two other numbers
+#
+# num - The integer number to check between two other number
+# first - The first integer number to check between
+# second - The second integer number to check between
+#
+# Examples
+#
+#   is_between(3,4,1)
+#   # => true
+#
+#   is_between(5,3,6)
+#   # => true
+#
+# Returns if the integer number is between the other two integers
+def is_between(num,first,second)
+    if num >= from && num <= to || num <= from && num >= to
+        return true
+    end
+    return false
+end
